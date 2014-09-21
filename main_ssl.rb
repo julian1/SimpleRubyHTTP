fa67c8dc0c9ae39db8f092a1cce469c32b31cb1f
julian1 <julian.atkinson71@gmail.com>
2014-09-21 16:59:49 +1000

    add certs and ssl stuff

---
+++ main_ssl.rb
@@ -0,0 +1,38 @@
+#require 'socket' # Provides TCPServer and TCPSocket classes
+#require 'openssl' 
+
+
+
+require "socket"
+require "openssl"
+require "thread"
+
+listeningPort = 1443 #Integer(ARGV[0])
+
+server = TCPServer.new(listeningPort)
+sslContext = OpenSSL::SSL::SSLContext.new
+#sslContext.cert = OpenSSL::X509::Certificate.new(File.open("cert.pem"))
+#sslContext.key = OpenSSL::PKey::RSA.new(File.open("priv.pem"))
+sslContext.cert = OpenSSL::X509::Certificate.new(File.open("server.crt"))
+sslContext.key = OpenSSL::PKey::RSA.new(File.open("server.key"))
+sslServer = OpenSSL::SSL::SSLServer.new(server, sslContext)
+
+puts "Listening on port #{listeningPort}"
+
+loop do
+  connection = sslServer.accept
+  Thread.new {
+    begin
+      while (lineIn = connection.gets)
+        lineIn = lineIn.chomp
+        $stdout.puts "=> " + lineIn
+        lineOut = "You said: " + lineIn
+        $stdout.puts "<= " + lineOut
+        connection.puts lineOut
+      end
+    rescue
+      $stderr.puts $!
+    end
+  }
+end
+

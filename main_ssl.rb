cff0c69595c2821b61f6a9755b8c92294df1093b
julian1 <julian.atkinson71@gmail.com>
2014-09-21 19:46:08 +1000

    update

--- main_ssl.rb
+++ main_ssl.rb
@@ -26,7 +26,11 @@ def decode_message( socket )
 end
 
 
-def write_hello_message( socket )
+def write_hello_message( keys, socket )
+
+      puts "write_hello keys"
+      puts keys
+
       response = "- Hello World!\n"
 
       # We need to include the Content-Type and Content-Length headers
@@ -48,7 +52,7 @@ def write_hello_message( socket )
 end
 
 
-def write_redirect_message( socket )
+def write_redirect_message( keys, socket )
 
   response = <<-EOS  
     <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
@@ -62,10 +66,12 @@ def write_redirect_message( socket )
     </body></html>
   EOS
  
+#    "Location: https://imos.aodn.org.au/imos123\r\n" + 
+
   socket.print "HTTP/1.1 302 Found\r\n" + 
     "Date: Sun, 21 Sep 2014 09:02:16 GMT\r\n" + 
     "Server: Apache\r\n" +
-    "Location: https://imos.aodn.org.au/imos123\r\n" + 
+    "Location: https://localhost:1443\r\n" + 
     "Vary: Accept-Encoding\r\n" + 
     "Content-Length: 282\r\n" +
     "Content-Type: text/html; charset=iso-8859-1\r\n"
@@ -91,22 +97,30 @@ def process_accept( server, &code )
       # if we don't spawn the thread then we get a broken pipe which is weird
     Thread.new {
       begin
+  
+        # we can decode the keys in here, ? we should do this
+        # so we can abstract session management
 
         # what's the terminator of the message...
         # should parse this into key value pairs... 
         # Decode message
         puts "--------------------"
         keys = decode_message( socket) 
-        puts keys
+        # puts keys
 
-        code.call( socket )
+        code.call( keys, socket )
 
 #        write_redirect_message( socket )
         #write_hello_message( socket )
 
         # Close the socket, terminating the connection
         # do we really need to do this ??
+
+        puts "before socket close"
+
         socket.close
+
+        puts "after socket close"
 
       rescue
         $stderr.puts $!
@@ -119,7 +133,7 @@ end
 
 threads = []
 
-
+# we want to abstract the starting of the servers ...
 
 threads << Thread.new {
   begin
@@ -135,8 +149,8 @@ threads << Thread.new {
 
     puts "Listening on port #{listeningPort}"
 
-    process_accept sslServer do |socket| 
-      write_hello_message( socket )
+    process_accept sslServer do |keys, socket| 
+      write_hello_message( keys, socket )
         # write_redirect_message( socket )
       end
 
@@ -152,8 +166,10 @@ threads << Thread.new {
     listeningPort = 2345 #Integer(ARGV[0])
     server2 = TCPServer.new('localhost', listeningPort)
     puts "Listening on port #{listeningPort}"
-    process_accept server2 do  |socket| 
-        write_redirect_message( socket )
+    process_accept server2 do  |keys, socket| 
+
+        write_hello_message( keys, socket )
+        #write_redirect_message( keys, socket )
       end
   rescue
     $stderr.puts $!

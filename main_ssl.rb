4207a4c8de1e67dd236d4385e671e36a7b6d2eae
julian1 <julian.atkinson71@gmail.com>
2014-09-21 17:16:23 +1000

    ruby add ssl processing

--- main_ssl.rb
+++ main_ssl.rb
@@ -20,16 +20,62 @@ sslServer = OpenSSL::SSL::SSLServer.new(server, sslContext)
 puts "Listening on port #{listeningPort}"
 
 loop do
-  connection = sslServer.accept
+  socket = sslServer.accept
   Thread.new {
     begin
-      while (lineIn = connection.gets)
-        lineIn = lineIn.chomp
-        $stdout.puts "=> " + lineIn
-        lineOut = "You said: " + lineIn
-        $stdout.puts "<= " + lineOut
-        connection.puts lineOut
+
+      # what's the terminator of the message...
+      # should parse this into key value pairs... 
+      # Decode message
+      puts "--------------------"
+      i = 0
+      keys = {}
+      while line = socket .gets# Read lines from socket
+        break if line == "\r\n"
+        if i == 0 
+          keys['request'] = line.strip
+        else 
+        s = line.split(':')
+        keys[ s[0].strip] = s[1].strip 
+        end
+        i += 1
       end
+
+      puts keys
+      puts "here2"
+
+  response = "Hello World!\n"
+
+  # We need to include the Content-Type and Content-Length headers
+  # to let the client know the size and type of data
+  # contained in the response. Note that HTTP is whitespace
+  # sensitive, and expects each header line to end with CRLF (i.e. "\r\n")
+  socket.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Content-Length: #{response.bytesize}\r\n" +
+                "Set-Cookie: name=whoot\r\n" +
+               "Connection: close\r\n"
+
+  # Print a blank line to separate the header from the response body,
+  # as required by the protocol.
+  socket.print "\r\n"
+
+  # Print the actual response body, which is just "Hello World!\n"
+  socket.print response
+
+  # Close the socket, terminating the connection
+  # do we really need to do this ??
+  socket.close
+
+
+
+#       while (lineIn = connection.gets)
+#         lineIn = lineIn.chomp
+#         $stdout.puts "=> " + lineIn
+#         lineOut = "You said: " + lineIn
+#         $stdout.puts "<= " + lineOut
+#         connection.puts lineOut
+#       end
     rescue
       $stderr.puts $!
     end

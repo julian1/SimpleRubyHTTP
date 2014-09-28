d10bc7c00408478f9561d2770e1855e6cebbda1a
julian1 <julian.atkinson71@gmail.com>
2014-09-28 14:44:57 +1000

    update

--- server.rb
+++ server.rb
@@ -1,17 +1,5 @@
 #!/usr/bin/ruby
 
-# Simple webserver example, 
-# supports connection keep alive, cookies, https and redirection 
-# need cache control
-# websockets,
-
-
-# there's an issue - if try http on 1443 - it goes into unknown state
-# https://localhost:1443/
-
-# wget --no-check-certificate 'http://localhost:2345'
-
-
 require "socket"
 require "openssl"
 require "thread"
@@ -23,16 +11,14 @@ def Server.process_accept( server, &code )
   loop do
     socket = server.accept
 
-    puts "** new connection"
+    $stderr.puts "** new connection"
 
-      # if we don't spawn the thread then we get a broken pipe which is weird
+    # if we don't spawn the thread then we get a broken pipe which is weird
     Thread.new {
       loop do
         begin
-          # we can decode the keys in here, ? we should do this
-          # so we can abstract session management
 
-          # puts keys
+          # call handler block
           break if code.call( socket ).nil?
 
 #         # i think we get a broken pipe if we can't read anything
@@ -46,10 +32,6 @@ def Server.process_accept( server, &code )
 #           socket.close
 #           break
 # 
-#         #rescue
-#           # Exception Broken pipe is normal when client disconnects - eg. when 302 disconnect 
-# 
-          
         rescue => e
           $stderr.puts "Error during processing: #{$!}"
           $stderr.puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"

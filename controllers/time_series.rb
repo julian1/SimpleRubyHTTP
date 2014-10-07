df0572343f095bb46fbc967e2102533dc76f02d6
julian1 <julian.atkinson71@gmail.com>
2014-10-07 22:21:37 +1100

    update

--- controllers/time_series.rb
+++ controllers/time_series.rb
@@ -10,8 +10,8 @@ def decode_url_args( capture )
     # ok, now we want to split the keyvalue pairs
 
     fields = {}
-    query.split('&').each do |pair| 
-      id, val = pair.split('=') 
+    query.split('&').each do |pair|
+      id, val = pair.split('=')
       fields[id] = val
     end
     fields
@@ -29,19 +29,19 @@ class TimeSeriesController
 	return if x[:response]
 
     # the ? isn't quite right here, it shouldn't be optional
-    # if there's a param set. 
+    # if there's a param set.
     matches = /^GET \/get_all_series.json\??(.*)$/.match(x[:request])
     if matches && matches.captures.length == 1
       #puts "raw query string #{ matches.captures[0] }"
       fields = decode_url_args( matches.captures[0])
       #puts "fields #{fields}"
-      # defaults 
+      # defaults
       ticks = fields['ticks'] ? fields['ticks'].to_i : 100
       interval = fields['interval'] ? fields['interval'].to_i : 1
       #puts "model length #{@model.length }"
       # @model.get_series( x, ticks)
       # take up to 500 elts, with logic to handle fewer
-      take = ticks 
+      take = ticks
       n = @model.length
       m = @model[ (n - take > 0 ? n - take : 0) .. n - 1]
       top_ask = m.map do |row| <<-EOF
@@ -64,11 +64,28 @@ class TimeSeriesController
     end
 
 
+    # an individual series
     matches = /^GET \/get_series.json\??(.*)$/.match(x[:request])
     if matches && matches.captures.length == 1
-       fields = decode_url_args( matches.captures[0])
-	      puts "fields #{fields}"
-
+      fields = decode_url_args( matches.captures[0])
+      puts "fields #{fields}"
+      ticks = fields['ticks'] ? fields['ticks'].to_i : 100
+      take = ticks
+      n = @model.length
+      m = @model[ (n - take > 0 ? n - take : 0) .. n - 1]
+      top_ask = m.map do |row| <<-EOF
+        { "id": "#{row[:id]}",
+          "time": "#{row[:time]}",
+          "value": #{row[:top_ask]} }
+        EOF
+      end
+      ret = <<-EOF
+        [ #{top_ask.join(", ")} ]
+      EOF
+      # puts ret
+      x[:response] = "HTTP/1.1 200 OK"
+      x[:response_headers]['Content-Type'] = "application/json"
+      x[:body] = StringIO.new( ret, "r")
     end
 
 
@@ -76,7 +93,7 @@ class TimeSeriesController
 
     # this whole id thing, where client submits id to check for state change, is
     # almost equivalent to etag approach
-		
+
 	# it's a substitute for haskell type laziness - where we just request.
     if /^GET \/get_id.json$/.match(x[:request])
 #      @model.get_id( x )

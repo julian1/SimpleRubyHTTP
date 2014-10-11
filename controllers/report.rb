02ee20460f210170e007c6d6cbbf1fcfde50e436
julian1 <julian.atkinson71@gmail.com>
2014-10-11 19:40:06 +1100

    got json being returned from query

--- controllers/report.rb
+++ controllers/report.rb
@@ -1,4 +1,15 @@
 
+require 'json'
+
+
+def map_query( conn, query, args, &code )
+	# map a proc/block over postgres query result
+	xs = []
+	conn.exec( query, args ).each do |row|
+	xs << code.call( row )
+	end
+	xs
+end
 
 
 class ReportController
@@ -26,14 +37,28 @@ class ReportController
 
         # we should return json, or decode the json
         res = @conn .exec_params( query )
-        w = StringIO.new()
-        res.each do |row|
-            w.puts row
-        end
+
+# 		puts 'here0'
+# 
+		rows = res.map do |row|
+			row.to_json
+		end
+
+# 		puts 'here1'
+# 		puts rows
+# 		puts 'here1-5'
+# 
+		s = " [ #{rows.join(', ')} ]"
+
+# 		puts 'here2'
+# 
+# 		puts s
+# 
+
         #   @log.info( "result is #{ w.string } ")
         x[:response] = "HTTP/1.1 200 OK"
         x[:response_headers]['Content-Type'] = "text/plain"
-        x[:body] = StringIO.new( w.string )  # we shouldn't need this double handling
+        x[:body] = StringIO.new( s )  # we shouldn't need this double handling
       else
         x[:response] = "HTTP/1.1 200 OK"
         x[:response_headers]['Content-Type'] = "text/plain"

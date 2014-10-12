43a75a29089b6a500c0d52a8dd3ed7d766a0602b
julian1 <julian.atkinson71@gmail.com>
2014-10-12 10:49:49 +0000

    no logging redirect

--- controllers/redirect.rb
+++ controllers/redirect.rb
@@ -13,7 +13,7 @@ class RedirectController
 
   def action( x)
     unless x[:socket].is_a?( OpenSSL::SSL::SSLSocket)
-      @log.info( "redirect to https" )
+      # @log.info( "redirect to https" )
       x[:response] = "HTTP/1.1 302 Found"
       x[:response_headers]['Location'] = "https://#{@hostname}:#{@port}"
     end

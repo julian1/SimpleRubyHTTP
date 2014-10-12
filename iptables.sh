d4a90402bb79ccdd43c84f582187f2e218b1aac5
julian1 <julian.atkinson71@gmail.com>
2014-10-12 10:50:51 +0000

    iptables to use 8000 and 8443 same as new server

--- iptables.sh
+++ iptables.sh
@@ -5,7 +5,7 @@ iptables -t nat -X
 
 # this won't work if it's not an external connection
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80  -j REDIRECT --to-port 8000
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8001
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8443
 
 iptables -t nat -L
 
